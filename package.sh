b227a9c9580aaacaff82e85fca6095ca5ba18cf7
bvmensvoort <info@bvmensvoort.nl>
2019-08-20 21:50:08 +0200

    Include package-lock generation in packaging the tarball

--- package.sh
+++ package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-rm -f SHA256SUMS
-sha256sum package.json *.js LICENSE > SHA256SUMS
+rm -f SHA256SUMS package-lock.json
+npm install --production --package-lock
+sha256sum package.json *.js lib/*.js LICENSE > SHA256SUMS
 npm pack
