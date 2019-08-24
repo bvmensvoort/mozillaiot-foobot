b2ce7775eaf2abcd48c5cfa4d643b5db6263f100
bvmensvoort <info@bvmensvoort.nl>
2019-08-24 13:27:44 +0200

    Add SHA256 checksum of built files

--- package.sh
+++ package.sh
@@ -4,3 +4,6 @@ rm -f SHA256SUMS package-lock.json
 npm install --production --package-lock
 sha256sum package.json index.js lib/*.js LICENSE > SHA256SUMS
 npm pack
+
+echo "-- For convenience when updating file in add-on list."
+sha256sum *.tgz
