c995ef86ded1a22e39cf5ff532c8442a00c6999c
bvmensvoort <info@bvmensvoort.nl>
2019-08-25 20:11:32 +0200

    Add dependency (node-module: request) to adapter and integrate into build

--- package.sh
+++ package.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
+echo "-- Resetting environment"
+rm -rf node_modules
+rm -f SHA256SUMS package-lock.json *.tgz
 
-rm -f SHA256SUMS package-lock.json
+echo "-- Installing dependencies and creating package-lock"
 npm install --production --package-lock
-sha256sum package.json index.js lib/*.js LICENSE > SHA256SUMS
-npm pack
 
-echo "-- For convenience when updating file in add-on list."
+echo "-- Generate SHA256 checksums"
+sha256sum package.json index.js lib/*.js LICENSE > SHA256SUMS
+find node_modules -type f -exec sha256sum {} \; >> SHA256SUMS
+
+echo "-- Pack to tar archive"
+TARFILE=$(npm pack)
+
+echo "-- Add dependent node_modules to archive"
+tar --extract --ungzip --file=${TARFILE}
+cp -r node_modules ./package
+tar --create --gzip --file=${TARFILE} ./package
+rm -rf package
+
+echo "-- Show SHA265 checksum of package"
 sha256sum *.tgz
